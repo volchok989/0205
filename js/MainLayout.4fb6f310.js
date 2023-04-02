3fc214e49aef30858354c8e469024e5772d280d1
Andrii Volkov <volkov989989@gmail.com>
2023-04-02 14:23:09 +0300

    Create deploy

---
+++ js/MainLayout.4fb6f310.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk_0205"]=self["webpackChunk_0205"]||[]).push([[585],{770:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=function(){var n=this,u=n._self._c;return u("router-view")},r=[],a={name:"MainLayout"},l=a,s=e(1),c=(0,s.Z)(l,t,r,!1,null,"65b07d2b",null),f=c.exports}}]);
+//# sourceMappingURL=MainLayout.4fb6f310.js.map
